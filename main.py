e803c8f2c7cccd1dd486c0e2c72be195ce03cb75
Nikita Yurchenko <n.yurchenko@vk.team>
2025-01-25 20:56:37 +0300

    Replaced with telethon

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-import asyncio
-import logging
 import os
-import sys
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
 from dotenv import load_dotenv
+from telethon import TelegramClient
+import telethon
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
 
-dp = Dispatcher()
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+@bot.on(telethon.events.NewMessage(pattern='/start'))
+async def handle_start(event: telethon.events.NewMessage):
+    pass
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Nice try!")
-
-async def main() -> None:
-    bot = Bot(token= BOT_TOKEN, default = DefaultBotProperties(parse_mode=ParseMode.HTML))
-    await dp.start_polling(bot)
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-    asyncio.run(main())
+bot.start()
+bot.run_until_disconnected()
