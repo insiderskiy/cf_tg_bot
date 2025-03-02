95c78f9aab348854f26c50a4d64f26d19647849c
Nikita Yurchenko <n.yurchenko@vk.team>
2025-03-02 14:39:26 +0300

    - Generate results is done

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import globals as g
 
@@ -19,10 +20,11 @@ async def clear_all():
         message_ids=await __get_all_msgs(g.CHAT_WITH_RESULTS_ID)
     )
 
-async def generate_complexes_with_results():
+
+async def __post_complexes():
     with open("test_data/complexes.json") as f:
         complexes = json.load(f)
-        for complex in complexes:
+        for complex in complexes[:3]:
             if complex.get('is_time', False):
                 t = "time"
             else:
@@ -39,3 +41,48 @@ async def generate_complexes_with_results():
                 parse_mode='html',
                 link_preview=True
             )
+
+
+def __get_results():
+    with open('test_data/results.json') as f:
+        results = json.load(f)
+        return results
+
+
+async def __get_complex_messages(results):
+    complex_message_dict = {}
+    complex_ids = list(set(map(lambda r: r['complex_id'], results)))
+    async for msg in g.app.iter_messages(g.CHANNEL_WITH_COMPLEXES):
+        try:
+            parts = msg.text.split("\u00A0\n\n")
+            complex_id_in_msg = int(parts[0].replace('ID: **', '').replace('**', ''))
+            if complex_id_in_msg in complex_ids:
+                complex_message_dict[complex_id_in_msg] = msg
+                if len(complex_message_dict) == len(complex_ids):
+                    break
+        except Exception as e:
+            print(e)
+    return complex_message_dict
+
+
+async def __post_results_by_complex_id(complex_id, complex_message, results):
+    complex_results = filter(lambda r: int(r['complex_id']) == complex_id, results)
+    for complex_result in complex_results:
+        text = (f"\n[{complex_result['username']}](tg://user?id={complex_result['user_id']})\n\n"
+         f"Результат: {complex_result['result']}")
+
+        await g.app.send_file(
+            entity=g.CHANNEL_WITH_COMPLEXES,
+            file=f"{os.getcwd()}/test_data/test_video.mp4",
+            caption=text,
+            parse_mode='markdown',
+            comment_to=complex_message.id
+        )
+
+
+async def generate_complexes_with_results():
+    await __post_complexes()
+    results = __get_results()
+    complex_messages = await __get_complex_messages(results)
+    for complex_id in complex_messages:
+        await __post_results_by_complex_id(complex_id, complex_messages[complex_id], results)
